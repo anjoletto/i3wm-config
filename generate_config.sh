83a6106d7e36793b84082eab841e1ecf3e589183
Leonardo <43471239+anjoletto@users.noreply.github.com>
2021-04-24 19:22:46 -0300

    script to combine files and save in the correct place

---
+++ generate_config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+INPUT_FILES=(i3_config appearance i3-gaps_config xinit bidings)
+
+OUTPUT_DIR=$HOME/.config/i3
+OUTPUT_FILE=$OUTPUT_DIR/config
+
+mkdir $OUTPUT_DIR
+rm $OUTPUT_FILE
+cat $INPUT_FILES > $OUTPUT_FILE
