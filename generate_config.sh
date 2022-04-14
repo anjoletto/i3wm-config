c886cab515c5b7aacc11388745b6ea30d48d51b9
Leonardo Anjoletto Ferreira <43471239+anjoletto@users.noreply.github.com>
2022-04-14 18:03:53 -0300

    updated config

--- generate_config.sh
+++ generate_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-#INPUT_FILES=(i3_config appearance i3-gaps_config xinit bidings)
-INPUT_FILES=(i3_config appearance xinit bidings)
+INPUT_FILES=(i3_config appearance i3-gaps_config xinit bidings)
+#INPUT_FILES=(i3_config appearance xinit bidings)
 
 OUTPUT_DIR=$HOME/.config/i3
 OUTPUT_FILE=$OUTPUT_DIR/config
